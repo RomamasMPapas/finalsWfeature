adafbb5a5d46eb5b2acf37a7e6f22b9df4000681
RomamasMPapas <robmalik191@gmail.com>
2025-12-04 13:22:58 +0800

    Add Bootstrap JS for dropdown functionality

--- gatsby-frontend/gatsby-browser.js
+++ gatsby-frontend/gatsby-browser.js
@@ -6,3 +6,10 @@
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./src/styles/custom.css"
+
+// Load Bootstrap JS for dropdowns, modals, etc.
+export const onClientEntry = () => {
+    if (typeof window !== "undefined") {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js")
+    }
+}
