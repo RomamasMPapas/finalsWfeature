1ed5562873846cdd28475218869d2308cd12fcf7
RomamasMPapas <robmalik191@gmail.com>
2025-12-04 09:56:08 +0800

    Remove broken firebase import from gatsby-browser.js

--- gatsby-frontend/gatsby-browser.js
+++ gatsby-frontend/gatsby-browser.js
@@ -5,11 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-// gatsby-browser.js
-// This file runs on every client page load
-
-// Import the Firebase app we created in src/firebase.js
-import { firebaseApp, analytics } from "./src/firebase";
-
-// You can now use `firebaseApp` or `analytics` anywhere in your components
-// (e.g., for logging events, auth, etc.)
