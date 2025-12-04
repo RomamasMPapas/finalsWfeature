e1cce1b3b06c30aab981af85e2e355b3e6128ca2
RomamasMPapas <robmalik191@gmail.com>
2025-12-04 12:28:20 +0800

    Fix gatsby-config - remove netlify plugin

--- gatsby-frontend/gatsby-config.js
+++ gatsby-frontend/gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `E-Commerce Store`,
+    description: `Shop the best products at unbeatable prices.`,
+    author: `@RomamasMPapas`,
+    siteUrl: `https://finals-wfeature-za9s.vercel.app/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -28,17 +28,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `E-Commerce Store`,
+        short_name: `E-Commerce`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    `gatsby-plugin-netlify`,
   ],
 }
